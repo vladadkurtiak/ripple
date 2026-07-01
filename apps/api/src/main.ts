c0d0bbfbcfae95597f1dc8fca317a0e2bb894b9a
Vladyslav Kurtiak <kurtakvladyslav@gmail.com>
2026-07-01 21:09:52 +0300

    feat: setup

---
+++ apps/api/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3002;
+  await app.listen(port);
+  console.log(`API is running on http://localhost:${port}`);
+}
+bootstrap();
