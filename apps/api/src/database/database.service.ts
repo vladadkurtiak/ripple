fc20f991224dac9fe1e0899994adac7faaf37ea3
Vladyslav Kurtiak <kurtakvladyslav@gmail.com>
2026-07-01 21:46:56 +0300

    feat: setup database

---
+++ apps/api/src/database/database.service.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from "@nestjs/common";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+
+@Injectable()
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(DatabaseService.name);
+
+  constructor() {
+    super({
+      adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log("Connected to the database");
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
